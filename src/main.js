80b2a85777793e580ffc17b26370dc312f43c96d
Dário Santos <dariobennaia@gmail.com>
2019-06-01 23:02:05 -0300

    Removendo biblioteca semantic ui e instalando bootstrap e axios

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import SuiVue from 'semantic-ui-vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 Vue.config.productionTip = false
-
-Vue.use(SuiVue);
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
