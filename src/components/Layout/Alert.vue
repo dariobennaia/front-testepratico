b621120bb6044c763859173eaed3d94f24c206cc
Dário Santos <dariobennaia@gmail.com>
2019-06-02 18:28:37 -0300

    Criação do componente do layout

---
+++ src/components/Layout/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="'alert alert-'+type" role="alert">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: {
+    'type': {
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Layout/InputMoneyFormGroup.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="'form-group col-'+col">
+    <label :for="id">{{title}}</label>
+    <input type="text" class="form-control" :id="id" :name="id" :placeholder="title" autocomplete="off" ref="precoImovel" v-model="valorPrecoImovel">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputMoneyFormGroup',
+  props: {
+    'title': {
+      required: true
+    },
+    'id': {
+      required: true
+    },
+    'col': {
+      required: true
+    },
+    'value': {
+      required: false,
+      default: null
+    }
+  },
+  data () {
+    return {
+      meu_valor: '0.00'
+    }
+  },
+  computed: {
+    valorPrecoImovel: {
+      // getter
+      get: function() {
+        return this.meu_valor;
+      },
+      // setter
+      set: function(newValue) {
+        this.meu_valor = this.formatPrice(newValue);
+      }
+    }
+  },
+  methods: {
+    formatPrice (value) {
+      const val = Number(value.replace(".", ""));
+      // if (!val) return value;
+      const valueString = val.toFixed(2);
+      return valueString.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
